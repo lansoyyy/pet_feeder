a4a4af121f8e012a8c83d7e9c6b7840f94e2a4a4
Lans12345 <olanalans12345@gmail.com>
2024-12-28 08:17:57 +0800

    added schedule feed ui

--- lib/screens/schedule_feed.dart
+++ lib/screens/schedule_feed.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:pet_feeder/utils/colors.dart';
+import 'package:pet_feeder/widgets/button_widget.dart';
 import 'package:pet_feeder/widgets/drawer_widget.dart';
 import 'package:pet_feeder/widgets/text_widget.dart';
 
-class ScheduleFeedScreen extends StatelessWidget {
+class ScheduleFeedScreen extends StatefulWidget {
   const ScheduleFeedScreen({super.key});
 
+  @override
+  State<ScheduleFeedScreen> createState() => _ScheduleFeedScreenState();
+}
+
+class _ScheduleFeedScreenState extends State<ScheduleFeedScreen> {
+  bool isRepeated = false;
+
+  TimeOfDay? _selectedTime;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,11 +28,85 @@ class ScheduleFeedScreen extends StatelessWidget {
           color: Colors.white,
         ),
       ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                'Set Feeding Time',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () async {
+                        TimeOfDay? selectedTime = await showTimePicker(
+                          context: context,
+                          initialTime: TimeOfDay.now(),
+                        );
+                        if (selectedTime != null) {
+                          setState(() {
+                            _selectedTime = selectedTime;
+                          });
+                        }
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 16, horizontal: 16),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Text(
+                          _selectedTime != null
+                              ? 'Selected Time: ${_selectedTime!.format(context)}'
+                              : 'Select Time',
+                          style:
+                              TextStyle(fontSize: 16, color: Colors.grey[600]),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 32),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Repeat Daily',
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                  Switch(
+                    value: isRepeated,
+                    onChanged: (value) {
+                      setState(() {
+                        isRepeated = value;
+                      });
+                      // Handle switch change here
+                    },
+                  ),
+                ],
+              ),
+              const Spacer(),
+              Center(
+                child: ButtonWidget(
+                  onPressed: () {},
+                  label: 'Save',
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
