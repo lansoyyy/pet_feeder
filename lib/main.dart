d260511fa17ba36552ee010d09fac7999d005e1b
Lans12345 <olanalans12345@gmail.com>
2025-01-02 08:45:25 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pet_feeder/firebase_options.dart';
 import 'package:pet_feeder/screens/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'petfeeding-41649',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
