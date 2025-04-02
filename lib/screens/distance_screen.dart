dfa674bf46244270cbd512ab91142d9fd41bcff9
Lans12345 <olanalans12345@gmail.com>
2025-04-03 07:16:36 +0800

    added distance ui

--- lib/screens/distance_screen.dart
+++ lib/screens/distance_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:pet_feeder/utils/colors.dart';
 import 'package:pet_feeder/widgets/drawer_widget.dart';
 import 'package:pet_feeder/widgets/text_widget.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 class DistanceScreen extends StatelessWidget {
   const DistanceScreen({super.key});
@@ -18,6 +19,40 @@ class DistanceScreen extends StatelessWidget {
           fontSize: 18,
           color: Colors.white,
         ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          TextWidget(
+            text: 'Current Food Storage Distance:',
+            fontSize: 18,
+            fontFamily: 'Bold',
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          SfRadialGauge(axes: <RadialAxis>[
+            RadialAxis(minimum: 0, maximum: 250, ranges: <GaugeRange>[
+              GaugeRange(startValue: 0, endValue: 50, color: Colors.green),
+              GaugeRange(startValue: 50, endValue: 100, color: Colors.orange),
+              GaugeRange(startValue: 100, endValue: 150, color: Colors.red)
+            ], pointers: const <GaugePointer>[
+              NeedlePointer(value: 90)
+            ], annotations: <GaugeAnnotation>[
+              GaugeAnnotation(
+                  widget: Container(
+                      child: const Text('90.0',
+                          style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.bold))),
+                  angle: 90,
+                  positionFactor: 0.5)
+            ])
+          ])
+        ],
       ),
     );
   }
