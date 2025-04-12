41a739e851c36f5a841234f393b7cd04a7ed552c
Lans12345 <olanalans12345@gmail.com>
2025-04-12 10:36:16 +0800

    fixed table

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -91,7 +91,6 @@ class _HistoryScreenState extends State<HistoryScreen> {
                         Map<dynamic, dynamic> rdbValue = snapshot
                             .data!.snapshot.value as Map<dynamic, dynamic>;
 
-                        print(rdbValue);
                         return DataTable(
                             border: TableBorder.all(
                               color: Colors.black,
@@ -104,12 +103,35 @@ class _HistoryScreenState extends State<HistoryScreen> {
                               DataColumn(label: Text("Weight")),
                             ],
                             rows: [
-                              for (int i = 0; i < data.docs.length; i++)
+                              for (int i = 0; i < 1; i++)
                                 DataRow(cells: [
-                                  DataCell(Text(DateFormat.yMMMd()
-                                      .add_jm()
-                                      .format(
-                                          data.docs[i]['dateTime'].toDate()))),
+                                  DataCell(StreamBuilder<Object>(
+                                      stream: null,
+                                      builder: (context, snapshot) {
+                                        String rawTime =
+                                            rdbValue['feeding']['timestamp'];
+                                        int hour =
+                                            int.parse(rawTime.substring(0, 2));
+                                        int minute =
+                                            int.parse(rawTime.substring(2, 4));
+                                        int second =
+                                            int.parse(rawTime.substring(4, 6));
+
+                                        // Create DateTime (today's date, just for placeholder)
+                                        DateTime now = DateTime.now();
+                                        DateTime time = DateTime(
+                                            now.year,
+                                            now.month,
+                                            now.day,
+                                            hour,
+                                            minute,
+                                            second);
+
+                                        // Format only the time (e.g., 9:10 PM)
+                                        String formattedTime =
+                                            DateFormat.jm().format(time);
+                                        return Text(formattedTime);
+                                      })),
                                   DataCell(Text(
                                       '${rdbValue['distance']['distance']} - High')),
                                   DataCell(Text(
